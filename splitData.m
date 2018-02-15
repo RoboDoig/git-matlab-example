ceecd07eb305e3161626ad129b91433c67dbae73
RoboDoig <erskine94@gmail.com>
2018-02-15 16:41:38 +0100

    First function complete

--- splitData.m
+++ splitData.m
@@ -2,5 +2,7 @@ function [ control, test ] = splitData( dataSet )
 %SPLITDATA splits a standard AutonoMouse data set into two structures, one
 %for control animals, one for test animals based on NMDA parameter
 
+% write amazing code
+
 end
 
